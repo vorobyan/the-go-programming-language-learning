1587fc0c7205a78ccd4e29f6db7cb6ec92961cba
vorobyan <daniil.vorobev951@gmail.com>
2025-05-21 17:25:03 +0300

    reading 1.3 chapter and doing exercise 1.4

---
+++ dup1.go
@@ -0,0 +1,22 @@
+// dup1 выводит текст каждой строки, которая появляется в
+// стандартном вводе более одного раза, а также количество ее появлений
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
